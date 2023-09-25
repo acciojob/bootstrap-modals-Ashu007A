929f0f8da46c832538c592c7c377680eea899b2a
Ashish Kumar Ram <74091038+Ashu007A@users.noreply.github.com>
2023-09-25 14:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+
+		var myModal = new bootstrap.Modal(document.getElementById('newton-modal'), {
+            backdrop: 'static'
+        });
+
+        document.getElementById('open-modal').addEventListener('click', function () {
+            myModal.show();
+        });
